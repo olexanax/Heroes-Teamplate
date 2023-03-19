853280a83e12f34018b11cc0ae58ded1b765addb
Oleksandr Sylko <sylkoalex@gmail.com>
2023-03-19 23:07:52 +0200

    debug in build

--- src/components/heroesListItem/HeroesListItem.js
+++ src/components/heroesListItem/HeroesListItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import unknownHero from '../../assets/unknown-hero.jpg'
 
 
 const HeroesListItem = React.memo(({name, description, element, onDelete, id}) => {
@@ -24,7 +25,7 @@ const HeroesListItem = React.memo(({name, description, element, onDelete, id}) =
     return (
         <li 
             className={`card flex-row mb-4 shadow-lg text-white ${elementClassName}`}>
-            <img src="http://www.stpaulsteinbach.org/wp-content/uploads/2014/09/unknown-hero.jpg" 
+            <img src={unknownHero}
                  className="img-fluid w-25 d-inline" 
                  alt="unknown hero" 
                  style={{'objectFit': 'cover'}}/>
